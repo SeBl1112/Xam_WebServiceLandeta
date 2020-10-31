1a2c939b490b3535021993e44a68b790074c1f29
santiago <santiagolandeta567@gmail.com>
2020-10-30 21:47:26 -0500

    Agregar archivos de proyecto.

---
+++ Xam_WebServiceLandeta/Xam_WebServiceLandeta/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xam_WebServiceLandeta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accpet", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<DemoAPI>>(content);
+
+                ListDemo.ItemsSource = resultado;
+            }
+
+        }
+    }
+
+    public class DemoAPI
+    {
+        public int userId { get; set; }
+        public int id { get; set; }x
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    
+
+
+
+}
